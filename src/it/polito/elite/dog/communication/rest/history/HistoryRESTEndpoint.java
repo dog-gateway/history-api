042182f7b4877bcf0748be5571f06a668e4904f2
Dario Bonino <dario.bonino@polito.it>
2014-07-11 15:54:44 +0200

    first beta release, bulk insertion of all devices is not yet implemented

--- src/it/polito/elite/dog/communication/rest/history/HistoryRESTEndpoint.java
+++ src/it/polito/elite/dog/communication/rest/history/HistoryRESTEndpoint.java
@@ -3,20 +3,26 @@
  */
 package it.polito.elite.dog.communication.rest.history;
 
+import it.polito.elite.dog.addons.storage.EventDataPoint;
+import it.polito.elite.dog.addons.storage.EventDataStream;
 import it.polito.elite.dog.addons.storage.EventDataStreamSet;
 import it.polito.elite.dog.addons.storage.EventStore;
 import it.polito.elite.dog.addons.storage.EventStoreInfo;
 import it.polito.elite.dog.communication.rest.history.api.HistoryRESTApi;
 import it.polito.elite.dog.core.library.util.LogHelper;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.StringReader;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicReference;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.codehaus.jackson.map.AnnotationIntrospector;
@@ -47,6 +53,10 @@ public class HistoryRESTEndpoint implements HistoryRESTApi
 	// the instance-level mapper
 	private ObjectMapper mapper;
 
+	// the date parser
+	// prepare the parameters
+	SimpleDateFormat sdf;
+
 	/**
 	 * Constructor
 	 */
@@ -54,6 +64,8 @@ public class HistoryRESTEndpoint implements HistoryRESTApi
 	{
 		// initialize the atomic reference
 		this.eventStore = new AtomicReference<>();
+
+		this.sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSSXX");
 
 		// initialize the instance-wide object mapper
 		this.mapper = new ObjectMapper();
@@ -73,6 +85,10 @@ public class HistoryRESTEndpoint implements HistoryRESTApi
 		// make serializer use Jackson
 		this.mapper.getSerializationConfig()
 				.withAnnotationIntrospector(jackson);
+
+		// set the date format
+		this.mapper.configure(
+				SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
 
 	}
 
@@ -136,8 +152,8 @@ public class HistoryRESTEndpoint implements HistoryRESTApi
 	public String getAllDeviceParametricNotifications(String deviceId,
 			String startDate, String endDate, Integer offset, Integer limit)
 	{
-		return this.getEventData(deviceId, startDate, endDate, offset, limit,
-				null, EventDataType.MEASURE, EventType.NOTIFICATION);
+		return this.getDeviceEventData(deviceId, startDate, endDate, offset,
+				limit, null, EventDataType.MEASURE, EventType.NOTIFICATION);
 	}
 
 	@Override
@@ -145,38 +161,33 @@ public class HistoryRESTEndpoint implements HistoryRESTApi
 			String startDate, String endDate, Integer offset, Integer limit,
 			Boolean aggregate)
 	{
-		return this.getEventData(deviceId, startDate, endDate,
-				offset, limit, aggregate, EventDataType.NOTMEASURE, EventType.NOTIFICATION);
+		return this.getDeviceEventData(deviceId, startDate, endDate, offset,
+				limit, aggregate, EventDataType.NOTMEASURE,
+				EventType.NOTIFICATION);
 	}
 
-	
 	@Override
 	public String getAllDeviceContinuousStates(String deviceId,
 			String startDate, String endDate, Integer offset, Integer limit)
 	{
-		return this.getEventData(deviceId, startDate, endDate, offset, limit,
-				null, EventDataType.MEASURE, EventType.STATE);
+		return this.getDeviceEventData(deviceId, startDate, endDate, offset,
+				limit, null, EventDataType.MEASURE, EventType.STATE);
 	}
 
 	@Override
 	public String getAllDeviceDiscreteStates(String deviceId, String startDate,
 			String endDate, Integer offset, Integer limit, Boolean aggregate)
 	{
-		return this.getEventData(deviceId, startDate, endDate,
-				offset, limit, aggregate, EventDataType.NOTMEASURE, EventType.STATE);
+		return this.getDeviceEventData(deviceId, startDate, endDate, offset,
+				limit, aggregate, EventDataType.NOTMEASURE, EventType.STATE);
 	}
-	
-	
-	private String getEventData(String deviceId, String startDate,
+
+	private String getDeviceEventData(String deviceId, String startDate,
 			String endDate, Integer offset, Integer limit, Boolean aggregate,
 			EventDataType dataType, EventType type)
 	{
 		// The extracted notifications as JSON
 		String extractedNotificationsJSON = "";
-
-		// prepare the parameters
-		SimpleDateFormat sdf = new SimpleDateFormat(
-				"yyyy-MM-dd'T'HH:mm:ss.SSSXX");
 
 		// parse the dates if present
 		Date start = new Date(0); // default, starts from the epoch
@@ -184,12 +195,12 @@ public class HistoryRESTEndpoint implements HistoryRESTApi
 		{
 			try
 			{
-				start = sdf.parse(startDate);
+				start = this.sdf.parse(startDate);
 			}
 			catch (ParseException e)
 			{
 				this.logger.log(LogService.LOG_ERROR,
-						"Unable to parse the start date");
+						"Unable to parse the start date", e);
 				throw new WebApplicationException(Response.Status.BAD_REQUEST);
 			}
 		}
@@ -296,5 +307,430 @@ public class HistoryRESTEndpoint implements HistoryRESTApi
 		}
 		else
 			return extractedNotificationsJSON;
+	}
+
+	@Override
+	public String getDeviceSpecificParametricNotification(String deviceId,
+			String notificationName, String notificationParams,
+			String startDate, String endDate, Integer offset, Integer limit)
+	{
+		return this.getDeviceSpecificEventData(deviceId, notificationName,
+				notificationParams, startDate, endDate, offset, limit,
+				EventDataType.MEASURE, EventType.NOTIFICATION);
+	}
+
+	@Override
+	public String getDeviceSpecificParametricNotification(String deviceId,
+			String notificationName, String startDate, String endDate,
+			Integer offset, Integer limit)
+	{
+		return this.getDeviceSpecificEventData(deviceId, notificationName,
+				null, startDate, endDate, offset, limit, EventDataType.MEASURE,
+				EventType.NOTIFICATION);
+	}
+
+	@Override
+	public String getDeviceSpecificNonParametricNotification(String deviceId,
+			String notificationName, String startDate, String endDate,
+			Integer offset, Integer limit)
+	{
+
+		return this.getDeviceSpecificEventData(deviceId, notificationName,
+				null, startDate, endDate, offset, limit,
+				EventDataType.NOTMEASURE, EventType.NOTIFICATION);
+	}
+
+	@Override
+	public String getDeviceSpecificContinuousStates(String deviceId,
+			String stateName, String stateParams, String startDate,
+			String endDate, Integer offset, Integer limit)
+	{
+		return this.getDeviceSpecificEventData(deviceId, stateName,
+				stateParams, startDate, endDate, offset, limit,
+				EventDataType.MEASURE, EventType.STATE);
+	}
+
+	@Override
+	public String getDeviceSpecificContinuousStates(String deviceId,
+			String stateName, String startDate, String endDate, Integer offset,
+			Integer limit)
+	{
+		return this.getDeviceSpecificEventData(deviceId, stateName, null,
+				startDate, endDate, offset, limit, EventDataType.MEASURE,
+				EventType.STATE);
+	}
+
+	@Override
+	public String getDeviceSpecificDiscreteStates(String deviceId,
+			String stateName, String startDate, String endDate, Integer offset,
+			Integer limit)
+	{
+		return this.getDeviceSpecificEventData(deviceId, stateName, null,
+				startDate, endDate, offset, limit, EventDataType.NOTMEASURE,
+				EventType.STATE);
+	}
+
+	private String getDeviceSpecificEventData(String deviceId, String name,
+			String params, String startDate, String endDate, Integer offset,
+			Integer limit, EventDataType dataType, EventType type)
+	{
+		// The extracted notifications as JSON
+		String extractedNotificationsJSON = "";
+
+		// parse the dates if present
+		Date start = new Date(0); // default, starts from the epoch
+		if ((startDate != null) && (!startDate.isEmpty()))
+		{
+			try
+			{
+				start = this.sdf.parse(startDate);
+			}
+			catch (ParseException e)
+			{
+				this.logger.log(LogService.LOG_ERROR,
+						"Unable to parse the start date");
+				throw new WebApplicationException(Response.Status.BAD_REQUEST);
+			}
+		}
+
+		// parse the dates if present
+		Date end = new Date(); // default, the current time
+		if ((endDate != null) && (!endDate.isEmpty()))
+		{
+			try
+			{
+				end = sdf.parse(endDate);
+			}
+			catch (ParseException e)
+			{
+				this.logger.log(LogService.LOG_ERROR,
+						"Unable to parse the end date");
+				throw new WebApplicationException(Response.Status.BAD_REQUEST);
+			}
+		}
+
+		// if the offset is not specified set the default value of 0 , i.e.,
+		// start from the first result.
+		if (offset == null)
+			offset = 0;
+
+		// if the limit parameter is not specified, set the default value at
+		// EventStoreInfo.UNLIMITED_SIZE, i.e., do not limit result size.
+		if (limit == null)
+			limit = EventStoreInfo.UNLIMITED_SIZE;
+
+		// if the parameters are null replace with the empty string
+		if ((params == null) || (params.isEmpty()))
+			params = "";
+
+		try
+		{
+			EventDataStream events = new EventDataStream();
+
+			switch (type)
+			{
+				case NOTIFICATION:
+				{
+					switch (dataType)
+					{
+						case MEASURE:
+						{
+							events = this.eventStore.get()
+									.getSpecificDeviceParametricNotifications(
+											deviceId, name, params, start, end,
+											offset, limit);
+							break;
+						}
+						case NOTMEASURE:
+						{
+							events = this.eventStore
+									.get()
+									.getSpecificDeviceNonParametricNotifications(
+											deviceId, name, start, end, offset,
+											limit);
+							break;
+						}
+					}
+
+					break;
+				}
+				case STATE:
+				{
+					switch (dataType)
+					{
+						case MEASURE:
+						{
+							events = this.eventStore.get()
+									.getSpecificDeviceContinuousStates(
+											deviceId, name, params, start, end,
+											offset, limit);
+							break;
+						}
+						case NOTMEASURE:
+						{
+							events = this.eventStore.get()
+									.getSpecificDeviceDiscreteStates(deviceId,
+											name, start, end, offset, limit);
+							break;
+						}
+					}
+				}
+			}
+
+			extractedNotificationsJSON = this.mapper.writeValueAsString(events);
+		}
+		catch (IOException e)
+		{
+			this.logger.log(LogService.LOG_ERROR,
+					"Unable to compose the response message for the paramteric notifications of "
+							+ deviceId, e);
+		}
+
+		if (extractedNotificationsJSON.isEmpty())
+		{
+			// launch the exception responsible for sending the HTTP response
+			throw new WebApplicationException(Response.Status.NOT_FOUND);
+		}
+		else
+			return extractedNotificationsJSON;
+	}
+
+	@Override
+	public void insertSpecificDeviceParametricNotificationCSV(String deviceId,
+			String notificationName, String csvData)
+	{
+		this.insertDeviceSpecificEventData(deviceId, notificationName, null,
+				csvData, EventDataType.MEASURE, EventType.NOTIFICATION,
+				MediaType.TEXT_PLAIN);
+	}
+
+	@Override
+	public void insertSpecificDeviceParametricNotificationCSV(String deviceId,
+			String notificationName, String notificationParams, String csvData)
+	{
+		this.insertDeviceSpecificEventData(deviceId, notificationName,
+				notificationParams, csvData, EventDataType.MEASURE,
+				EventType.NOTIFICATION, MediaType.TEXT_PLAIN);
+
+	}
+
+	@Override
+	public void insertSpecificDeviceNonParametricNotificationCSV(
+			String deviceId, String notificationName, String csvData)
+	{
+		this.insertDeviceSpecificEventData(deviceId, notificationName, null,
+				csvData, EventDataType.NOTMEASURE, EventType.NOTIFICATION,
+				MediaType.TEXT_PLAIN);
+	}
+
+	@Override
+	public void insertSpecificDeviceContinuousStateCSV(String deviceId,
+			String stateName, String csvData)
+	{
+		this.insertDeviceSpecificEventData(deviceId, stateName, null, csvData,
+				EventDataType.MEASURE, EventType.STATE, MediaType.TEXT_PLAIN);
+	}
+
+	@Override
+	public void insertSpecificDeviceContinuousStateCSV(String deviceId,
+			String stateName, String stateParams, String csvData)
+	{
+		this.insertDeviceSpecificEventData(deviceId, stateName, stateParams,
+				csvData, EventDataType.MEASURE, EventType.STATE,
+				MediaType.TEXT_PLAIN);
+	}
+
+	@Override
+	public void insertSpecificDeviceDiscreteStateCSV(String deviceId,
+			String stateName, String csvData)
+	{
+		this.insertDeviceSpecificEventData(deviceId, stateName, null, csvData,
+				EventDataType.NOTMEASURE, EventType.STATE, MediaType.TEXT_PLAIN);
+	}
+
+	@Override
+	public void insertSpecificDeviceParametricNotificationJSON(String deviceId,
+			String notificationName, String jsonData)
+	{
+		this.insertDeviceSpecificEventData(deviceId, notificationName, null,
+				jsonData, EventDataType.MEASURE, EventType.NOTIFICATION,
+				MediaType.APPLICATION_JSON);
+	}
+
+	@Override
+	public void insertSpecificDeviceParametricNotificationJSON(String deviceId,
+			String notificationName, String notificationParams, String jsonData)
+	{
+		this.insertDeviceSpecificEventData(deviceId, notificationName,
+				notificationParams, jsonData, EventDataType.MEASURE,
+				EventType.NOTIFICATION, MediaType.APPLICATION_JSON);
+
+	}
+
+	@Override
+	public void insertSpecificDeviceNonParametricNotificationJSON(
+			String deviceId, String notificationName, String jsonData)
+	{
+		this.insertDeviceSpecificEventData(deviceId, notificationName, null,
+				jsonData, EventDataType.NOTMEASURE, EventType.NOTIFICATION,
+				MediaType.APPLICATION_JSON);
+	}
+
+	@Override
+	public void insertSpecificDeviceContinuousStateJSON(String deviceId,
+			String stateName, String jsonData)
+	{
+		this.insertDeviceSpecificEventData(deviceId, stateName, null, jsonData,
+				EventDataType.MEASURE, EventType.STATE,
+				MediaType.APPLICATION_JSON);
+	}
+
+	@Override
+	public void insertSpecificDeviceContinuousStateJSON(String deviceId,
+			String stateName, String stateParams, String jsonData)
+	{
+		this.insertDeviceSpecificEventData(deviceId, stateName, stateParams,
+				jsonData, EventDataType.MEASURE, EventType.STATE,
+				MediaType.APPLICATION_JSON);
+	}
+
+	@Override
+	public void insertSpecificDeviceDiscreteStateJSON(String deviceId,
+			String stateName, String jsonData)
+	{
+		this.insertDeviceSpecificEventData(deviceId, stateName, null, jsonData,
+				EventDataType.NOTMEASURE, EventType.STATE,
+				MediaType.APPLICATION_JSON);
+	}
+
+	private void insertDeviceSpecificEventData(String deviceId, String name,
+			String params, String data, EventDataType eventDataType,
+			EventType eventType, String mediaType)
+	{
+		// create the event stream
+		EventDataStream stream = new EventDataStream(name,
+				(params != null ? params : ""), deviceId);
+
+		// fill the stream
+		switch (mediaType)
+		{
+			case MediaType.APPLICATION_JSON:
+			{
+				fillStreamFromJSON(data, stream);
+				break;
+			}
+			case MediaType.TEXT_PLAIN:
+			{
+				fillStreamFromCSV(data, stream);
+				break;
+			}
+		}
+
+		if (!stream.getDatapoints().isEmpty())
+		{
+			EventDataStreamSet streamSet = new EventDataStreamSet(deviceId);
+			streamSet.addDatastream(stream);
+
+			switch (eventType)
+			{
+				case NOTIFICATION:
+				{
+					switch (eventDataType)
+					{
+						case MEASURE:
+						{
+							this.eventStore.get()
+									.insertParametricNotifications(streamSet);
+							break;
+						}
+						case NOTMEASURE:
+						{
+							this.eventStore
+									.get()
+									.insertNonParametricNotifications(streamSet);
+							break;
+						}
+					}
+					break;
+				}
+				case STATE:
+				{
+					switch (eventDataType)
+					{
+						case MEASURE:
+						{
+							this.eventStore.get().insertContinuousStates(
+									streamSet);
+							break;
+						}
+						case NOTMEASURE:
+						{
+							this.eventStore.get().insertDiscreteStates(
+									streamSet);
+							break;
+						}
+					}
+					break;
+				}
+			}
+
+		}
+
+	}
+
+	private void fillStreamFromJSON(String jsonData, EventDataStream stream)
+	{
+		try
+		{
+			// extract the set of datapoints to add
+			ArrayList<EventDataPoint> dataPoints = this.mapper.readValue(
+					jsonData,
+					mapper.getTypeFactory().constructCollectionType(
+							ArrayList.class, EventDataPoint.class));
+
+			// store the datapoints
+			stream.setDatapoints(dataPoints);
+		}
+		catch (IOException e)
+		{
+			this.logger.log(LogService.LOG_ERROR,
+					"Error while parsing the given json data", e);
+
+			throw new WebApplicationException(
+					Response.Status.BAD_REQUEST);
+		}
+	}
+
+	private void fillStreamFromCSV(String csvData, EventDataStream stream)
+	{
+		StringReader csvReader = new StringReader(csvData);
+		BufferedReader bufferedCsvReader = new BufferedReader(csvReader);
+
+		// iterate over csv lines
+		String line = "";
+		EventDataPoint point = null;
+		try
+		{
+			while ((line = bufferedCsvReader.readLine()) != null)
+			{
+				String data[] = line.trim().split(",");
+				if (data.length >= 3)
+				{
+					Date timestamp = this.sdf.parse(data[0]);
+					point = new EventDataPoint(timestamp, data[1], data[2]);
+
+					// add the point
+					stream.addDatapoint(point);
+				}
+			}
+		}
+		catch (IOException | ParseException e)
+		{
+			this.logger.log(LogService.LOG_ERROR,
+					"Error while parsing the given csv data", e);
+
+			throw new WebApplicationException(
+					Response.Status.BAD_REQUEST);
+		}
 	}
 }
--- src/it/polito/elite/dog/communication/rest/history/api/HistoryRESTApi.java
+++ src/it/polito/elite/dog/communication/rest/history/api/HistoryRESTApi.java
@@ -3,6 +3,9 @@
  */
 package it.polito.elite.dog.communication.rest.history.api;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -41,7 +44,8 @@ public interface HistoryRESTApi
 	 * 
 	 * @return The stored notifications, in JSON
 	 */
-	@Path("devices/{device-id}/notifications/parametric/all")
+	@GET
+	@Path("/devices/{device-id}/notifications/parametric/all")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getAllDeviceParametricNotifications(
 			@PathParam("device-id") String deviceId,
@@ -74,7 +78,8 @@ public interface HistoryRESTApi
 	 *            notification will be generated. (default, true)
 	 * @return
 	 */
-	@Path("devices/{device-id}/notifications/nonparametric/all")
+	@GET
+	@Path("/devices/{device-id}/notifications/nonparametric/all")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getAllDeviceNonParametricNotifications(
 			@PathParam("device-id") String deviceId,
@@ -83,10 +88,10 @@ public interface HistoryRESTApi
 			@QueryParam("offset") Integer offset,
 			@QueryParam("limit") Integer limit,
 			@QueryParam("aggregate") Boolean aggregate);
-	
+
 	/**
-	 * Provides the history of all continuous states associated to the
-	 * given device, identified by the given device id.
+	 * Provides the history of all continuous states associated to the given
+	 * device, identified by the given device id.
 	 * 
 	 * @param deviceId
 	 *            The URI of the device for which the history should be
@@ -105,7 +110,8 @@ public interface HistoryRESTApi
 	 * 
 	 * @return The stored notifications, in JSON
 	 */
-	@Path("devices/{device-id}/states/continuous/all")
+	@GET
+	@Path("/devices/{device-id}/states/continuous/all")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getAllDeviceContinuousStates(
 			@PathParam("device-id") String deviceId,
@@ -115,8 +121,8 @@ public interface HistoryRESTApi
 			@QueryParam("limit") Integer limit);
 
 	/**
-	 * Provides the history of all discrete states associated to
-	 * the given device, identified by the given device id.
+	 * Provides the history of all discrete states associated to the given
+	 * device, identified by the given device id.
 	 * 
 	 * @param deviceId
 	 *            The URI of the device for which the history should be
@@ -138,7 +144,8 @@ public interface HistoryRESTApi
 	 *            notification will be generated. (default, true)
 	 * @return
 	 */
-	@Path("devices/{device-id}/states/discrete/all")
+	@GET
+	@Path("/devices/{device-id}/states/discrete/all")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getAllDeviceDiscreteStates(
 			@PathParam("device-id") String deviceId,
@@ -147,4 +154,177 @@ public interface HistoryRESTApi
 			@QueryParam("offset") Integer offset,
 			@QueryParam("limit") Integer limit,
 			@QueryParam("aggregate") Boolean aggregate);
+
+	@GET
+	@Path("/devices/{device-id}/notifications/parametric/{notification-name}/{notification-params}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getDeviceSpecificParametricNotification(
+			@PathParam("device-id") String deviceId,
+			@PathParam("notification-name") String notificationName,
+			@PathParam("notification-params") String notificationParams,
+			@QueryParam("start") String startDate,
+			@QueryParam("end") String endDate,
+			@QueryParam("offset") Integer offset,
+			@QueryParam("limit") Integer limit);
+
+	@GET
+	@Path("/devices/{device-id}/notifications/parametric/{notification-name}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getDeviceSpecificParametricNotification(
+			@PathParam("device-id") String deviceId,
+			@PathParam("notification-name") String notificationName,
+			@QueryParam("start") String startDate,
+			@QueryParam("end") String endDate,
+			@QueryParam("offset") Integer offset,
+			@QueryParam("limit") Integer limit);
+
+	@GET
+	@Path("/devices/{device-id}/notifications/nonparametric/{notification-name}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getDeviceSpecificNonParametricNotification(
+			@PathParam("device-id") String deviceId,
+			@PathParam("notification-name") String notificationName,
+			@QueryParam("start") String startDate,
+			@QueryParam("end") String endDate,
+			@QueryParam("offset") Integer offset,
+			@QueryParam("limit") Integer limit);
+
+	@GET
+	@Path("/devices/{device-id}/states/continuous/{state-name}/{state-params}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getDeviceSpecificContinuousStates(
+			@PathParam("device-id") String deviceId,
+			@PathParam("state-name") String stateName,
+			@PathParam("state-params") String stateParams,
+			@QueryParam("start") String startDate,
+			@QueryParam("end") String endDate,
+			@QueryParam("offset") Integer offset,
+			@QueryParam("limit") Integer limit);
+
+	@GET
+	@Path("/devices/{device-id}/states/continuous/{state-name}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getDeviceSpecificContinuousStates(
+			@PathParam("device-id") String deviceId,
+			@PathParam("state-name") String stateName,
+			@QueryParam("start") String startDate,
+			@QueryParam("end") String endDate,
+			@QueryParam("offset") Integer offset,
+			@QueryParam("limit") Integer limit);
+
+	@GET
+	@Path("/devices/{device-id}/states/discrete/{state-name}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getDeviceSpecificDiscreteStates(
+			@PathParam("device-id") String deviceId,
+			@PathParam("state-name") String stateName,
+			@QueryParam("start") String startDate,
+			@QueryParam("end") String endDate,
+			@QueryParam("offset") Integer offset,
+			@QueryParam("limit") Integer limit);
+	
+	// -------------- CSV PUT -------------------------
+
+	@PUT
+	@Path("/devices/{device-id}/notifications/parametric/{notification-name}")
+	@Consumes(MediaType.TEXT_PLAIN)
+	public void insertSpecificDeviceParametricNotificationCSV(
+			@PathParam("device-id") String deviceId,
+			@PathParam("notification-name") String notificationName,
+			String csvData);
+
+	@PUT
+	@Path("/devices/{device-id}/notifications/parametric/{notification-name}/{notification-params}")
+	@Consumes(MediaType.TEXT_PLAIN)
+	public void insertSpecificDeviceParametricNotificationCSV(
+			@PathParam("device-id") String deviceId,
+			@PathParam("notification-name") String notificationName,
+			@PathParam("notification-params") String notificationParams,
+			String csvData);
+	
+	@PUT
+	@Path("/devices/{device-id}/notifications/nonparametric/{notification-name}")
+	@Consumes(MediaType.TEXT_PLAIN)
+	public void insertSpecificDeviceNonParametricNotificationCSV(
+			@PathParam("device-id") String deviceId,
+			@PathParam("notification-name") String notificationName,
+			String csvData);
+	
+	@PUT
+	@Path("/devices/{device-id}/states/continuous/{state-name}")
+	@Consumes(MediaType.TEXT_PLAIN)
+	public void insertSpecificDeviceContinuousStateCSV(
+			@PathParam("device-id") String deviceId,
+			@PathParam("state-name") String stateName,
+			String csvData);
+
+	@PUT
+	@Path("/devices/{device-id}/states/continuous/{state-name}/{state-params}")
+	@Consumes(MediaType.TEXT_PLAIN)
+	public void insertSpecificDeviceContinuousStateCSV(
+			@PathParam("device-id") String deviceId,
+			@PathParam("state-name") String stateName,
+			@PathParam("state-params") String stateParams,
+			String csvData);
+	
+	@PUT
+	@Path("/devices/{device-id}/states/discrete/{state-name}")
+	@Consumes(MediaType.TEXT_PLAIN)
+	public void insertSpecificDeviceDiscreteStateCSV(
+			@PathParam("device-id") String deviceId,
+			@PathParam("state-name") String stateName,
+			String csvData);
+	
+	// ------------ JSON PUT ------------------
+	
+	@PUT
+	@Path("/devices/{device-id}/notifications/parametric/{notification-name}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void insertSpecificDeviceParametricNotificationJSON(
+			@PathParam("device-id") String deviceId,
+			@PathParam("notification-name") String notificationName,
+			String jsonData);
+
+	@PUT
+	@Path("/devices/{device-id}/notifications/parametric/{notification-name}/{notification-params}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void insertSpecificDeviceParametricNotificationJSON(
+			@PathParam("device-id") String deviceId,
+			@PathParam("notification-name") String notificationName,
+			@PathParam("notification-params") String notificationParams,
+			String jsonData);
+	
+	@PUT
+	@Path("/devices/{device-id}/notifications/nonparametric/{notification-name}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void insertSpecificDeviceNonParametricNotificationJSON(
+			@PathParam("device-id") String deviceId,
+			@PathParam("notification-name") String notificationName,
+			String jsonData);
+	
+	@PUT
+	@Path("/devices/{device-id}/states/continuous/{state-name}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void insertSpecificDeviceContinuousStateJSON(
+			@PathParam("device-id") String deviceId,
+			@PathParam("state-name") String stateName,
+			String jsonData);
+
+	@PUT
+	@Path("/devices/{device-id}/states/continuous/{state-name}/{state-params}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void insertSpecificDeviceContinuousStateJSON(
+			@PathParam("device-id") String deviceId,
+			@PathParam("state-name") String stateName,
+			@PathParam("state-params") String stateParams,
+			String jsonData);
+	
+	@PUT
+	@Path("/devices/{device-id}/states/discrete/{state-name}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void insertSpecificDeviceDiscreteStateJSON(
+			@PathParam("device-id") String deviceId,
+			@PathParam("state-name") String stateName,
+			String jsonData);
+
 }
